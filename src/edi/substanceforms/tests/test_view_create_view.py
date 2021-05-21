ecb1580065e8a8a428992d554edbc850b7c4c568
Seppo Walther <seppo.walther@educorvi.de>
2021-05-21 10:03:13 +0200

    "Add view: create-view"

---
+++ src/edi/substanceforms/tests/test_view_create_view.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from edi.substanceforms.testing import EDI_SUBSTANCEFORMS_FUNCTIONAL_TESTING
+from edi.substanceforms.testing import EDI_SUBSTANCEFORMS_INTEGRATION_TESTING
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getMultiAdapter
+from zope.component.interfaces import ComponentLookupError
+
+import unittest
+
+
+class ViewsIntegrationTest(unittest.TestCase):
+
+    layer = EDI_SUBSTANCEFORMS_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        api.content.create(self.portal, 'Folder', 'other-folder')
+        api.content.create(self.portal, 'Document', 'front-page')
+
+    def test_create_view_is_registered(self):
+        view = getMultiAdapter(
+            (self.portal['other-folder'], self.portal.REQUEST),
+            name='create-view'
+        )
+        self.assertTrue(view.__name__ == 'create-view')
+        # self.assertTrue(
+        #     'Sample View' in view(),
+        #     'Sample View is not found in create-view'
+        # )
+
+    def test_create_view_not_matching_interface(self):
+        with self.assertRaises(ComponentLookupError):
+            getMultiAdapter(
+                (self.portal['front-page'], self.portal.REQUEST),
+                name='create-view'
+            )
+
+
+class ViewsFunctionalTest(unittest.TestCase):
+
+    layer = EDI_SUBSTANCEFORMS_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
