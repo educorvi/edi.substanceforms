849f17ff8d46532af59e4faf5d6f486021b3ee09
Lars Walther <meinkraeks@gmail.com>
2021-08-07 22:24:12 +0200

    Datenbank View

--- src/edi/substanceforms/views/datenbank_view.py
+++ src/edi/substanceforms/views/datenbank_view.py
@@ -3,15 +3,8 @@
 from edi.substanceforms import _
 from Products.Five.browser import BrowserView
 
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-
 class DatenbankView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('datenbank_view.pt')
 
     def __call__(self):
-        # Implement your own actions:
-        self.msg = _(u'A small message')
+        self.objectlist = self.context.listFolderContents(contentFilter={"portal_type" : "Datenbank"})
         return self.index()
