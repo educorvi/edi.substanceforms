21009d22bc69f6996b3756d5881bd3bf925bdd11
Lars Walther <meinkraeks@gmail.com>
2021-08-07 21:49:23 +0200

    Felder für die Content-Types Datenbank und Tabelle

--- src/edi/substanceforms/content/datenbank.py
+++ src/edi/substanceforms/content/datenbank.py
@@ -1,60 +1,45 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.app.textfield import RichText
 from plone.dexterity.content import Container
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-# from edi.substanceforms import _
+from edi.substanceforms import _
 
 
 class IDatenbank(model.Schema):
     """ Marker interface and Dexterity Python Schema for Datenbank
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('datenbank.xml')
+    adresse = schema.TextLine(
+            title = u"IP-Adresse oder DNS-Name der Datenbank"
+            )
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    portnummer = schema.TextLine(
+            title = u"Portnummer für die Verbindung zur Datenbank",
+            description = u"Keine Angabe entspricht Standardportnummer",
+            required = False
+            )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    username = schema.TextLine(
+            title = u"Benutzername für die Verbindung zur Datenbank"
+            )
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    password = schema.Password(
+            title = u"Passwort für die Verbindung zur Datenbank"
+            )
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
+    text = RichText(
+            title = "Text vor der Auflistung der Datenbanktabellen",
+            required = False
+            )
 
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    schlusstext = RichText(
+            title = u"Text nach der Auflistung der Datenbanktabellen",
+            required = False
+            )
 
 
 @implementer(IDatenbank)
--- src/edi/substanceforms/content/tabelle.py
+++ src/edi/substanceforms/content/tabelle.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.app.textfield import RichText
 from plone.dexterity.content import Container
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
@@ -16,45 +12,22 @@ from zope.interface import implementer
 class ITabelle(model.Schema):
     """ Marker interface and Dexterity Python Schema for Tabelle
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('tabelle.xml')
+    tablename = schema.TextLine(
+            title = u"Name der Datenbanktabelle",
+            description = u"Der Name der Datenbanktabelle wird nur für interne Zugriffe verwendet\
+                    und dem Benutzer nicht angezeigt."
+            )
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    text = RichText(
+            title = "Text vor dem View auf die Datenbanktabelle",
+            required = False
+            )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    schlusstext = RichText(
+            title = u"Text nach dem View auf die Datenbanktabelle",
+            required = False
+            )
 
 
 @implementer(ITabelle)
