fad435d81c262ac1e863c96f744c23e72dc7a593
Lars Walther <meinkraeks@gmail.com>
2021-08-09 15:04:29 +0200

    Single View

--- src/edi/substanceforms/views/single_view.py
+++ src/edi/substanceforms/views/single_view.py
@@ -13,6 +13,11 @@ class SingleView(BrowserView):
         self.username = self.context.aq_parent.username
         self.password = self.context.aq_parent.password
         self.article = self.get_article()
+        self.machines = []
+        self.secsheet = []
+        if self.context.tablename == 'substance_mixture':
+            self.machines = self.get_machines()
+            self.secsheet = self.get_recipes()
         return self.index()
 
     def get_article(self):
@@ -22,7 +27,51 @@ class SingleView(BrowserView):
         select = "SELECT * from %s WHERE %s_id = %s" %(tablename, tablename, self.itemid)
         cur.execute(select)
         article = cur.fetchall()
-        import pdb;pdb.set_trace()
         cur.close
         conn.close()
         return article
+
+    def get_machines(self):
+        machine_titles = []
+        conn = psycopg2.connect(host=self.host, user=self.username, dbname=self.dbname, password=self.password)
+        cur = conn.cursor()
+        select = "SELECT machine_id from mixture_pairs WHERE mixture_id = %s" %self.itemid
+        cur.execute(select)
+        machine_ids = cur.fetchall() #TODO: muss evenutell noch behandelt werden
+        cur.close()
+        for i in machine_ids:
+            cur = conn.cursor()
+            select = "SELECT title from printing_machine WHERE printing_machine_id = %s" %i
+            cur.execute(select)
+            machine_title = cur.fetchall()
+            cur.close()
+            machine_titles.append(machine_title)
+        conn.close()    
+        return machine_titles
+
+    def get_recipes(self):
+        substances = []
+        conn = psycopg2.connect(host=self.host, user=self.username, dbname=self.dbname, password=self.password)
+        cur = conn.cursor()
+        select = "SELECT subtance_id, concentration from recipes WHERE mixture_id = %s" %self.itemid
+        cur.execute(select)
+        substance_ids = cur.fetchall() #TODO: muss evenutell noch behandelt werden
+        cur.close()
+        for sid, concentration in substance_ids:
+            cur = conn.cursor()
+            select = "SELECT title from substance WHERE substance_id = %s" %sid
+            cur.execute(select)
+            substance_title = cur.fetchall()
+            cur.close()
+            entry = {'title':substance_title, 'concentration':concentration}
+            substances.append(entry)
+        conn.close()
+        return substances
+
+    def get_substance_type(self, substance_type):
+        substance_types = {'detergent_label': u'Reinigungsmittel Etiketten',
+                           'detergent_heatset': u'Heatsetwaschmittel',
+                           'detergent_manual' : u'Reinigungsmittel manueller Gebrauch',
+                           'product_datasheet': u'Wasch- und Reinigungsmittel für den Offsetdruck'}
+        return substance_types.get(substance_type)
+
