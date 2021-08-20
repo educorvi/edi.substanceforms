891e88dd3cbd8853dc15785b78565075075bd56b
Seppo Walther <seppo.walther@educorvi.de>
2021-08-20 10:33:31 +0200

    Working on migrationview.py

--- src/edi/substanceforms/views/migrationview.py
+++ src/edi/substanceforms/views/migrationview.py
@@ -21,29 +21,3 @@ class Migrationview(BrowserView):
         self.password = self.context.aq_parent.password
         import pdb; pdb.set_trace()
         return self.index()
-"""
-    def submit(self, button):
-        redirect_url = self.context.aq_parent.absolute_url()
-        if button == 'Speichern' and self.validate():
-
-            if True:
-                conn = psycopg2.connect(host=self.host, user=self.username, dbname=self.dbname, password=self.password)
-                cur = conn.cursor()
-                insert = """INSERT INTO manufacturer VALUES (DEFAULT, '%s', '%s', '%s', %s);""" % (self.form.title.data,
-                                                                                                   self.form.description.data,
-                                                                                                   self.context.aq_parent.get_webcode(),
-                                                                                                   check_value(
-                                                                                                       self.form.homepage.data))
-                cur.execute(insert)
-                conn.commit()
-                cur.close()
-                conn.close()
-                message = u'Der Hersteller wurde erfolgreich gespeichert.'
-                ploneapi.portal.show_message(message=message, type='info', request=self.request)
-            # except:
-            #    print(u'Fehler beim Einfügen in die Datenbank')
-            return self.request.response.redirect(redirect_url)
-
-        elif button == 'Abbrechen':
-            return self.request.response.redirect(redirect_url)
-"""
