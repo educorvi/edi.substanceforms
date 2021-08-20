f9eea60257a9091c743c5946f90ab4b3c0a6ae42
Seppo Walther <seppo.walther@educorvi.de>
2021-08-20 10:24:26 +0200

    Working on migrationview.py

--- src/edi/substanceforms/views/migrationview.py
+++ src/edi/substanceforms/views/migrationview.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from Products.Five.browser import BrowserView
+import requests
+import psycopg2
+import random
+import datetime
 
+import pdb; pdb.set_trace()
 
+"""
 class Migrationview(BrowserView):
     def __call__(self):
         template = '''<li class="heading" i18n:translate="">
           Sample View
         </li>'''
         return template
+"""
