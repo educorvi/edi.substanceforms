0a8a5e0e7b6ab66c1391749eec4e026d361200c8
Seppo Walther <seppo.walther@educorvi.de>
2021-10-04 15:16:10 +0200

    Working on generalized Form

--- src/edi/substanceforms/views/tabelle_view.py
+++ src/edi/substanceforms/views/tabelle_view.py
@@ -107,3 +107,17 @@ class SprayPowderForm (BaseForm):
 
 class HerstellerFormView(TabelleFormView):
     formClass = HerstellerForm
+
+    def renderForm(self):
+        try:
+            conn = psycopg2.connect(host=self.host, user=self.username, dbname=self.dbname, password=self.password)
+            cur = conn.cursor()
+            cur.execute("SELECT manufacturer_id, title FROM manufacturer;")
+            manus = cur.fetchall()
+            cur.close
+            conn.close()
+        except:
+            manus = []
+        self.form.manu.choices = manus
+        self.form.process()
+        return self.formTemplate()
