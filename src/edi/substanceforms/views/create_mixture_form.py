4796f508a2f1e89f0b77cf14a2ec88d9c08c0bce
Seppo Walther <seppo.walther@educorvi.de>
2021-10-07 10:55:38 +0200

    Fixing bug

--- src/edi/substanceforms/views/create_mixture_form.py
+++ src/edi/substanceforms/views/create_mixture_form.py
@@ -101,7 +101,7 @@ class CreateFormView(WTFormView):
                                                             self.form.values_range.data,
                                                             check_value(self.form.comments.data),
                                                             #self.form.usecases.data,
-                                                            {Test, jawoll},
+                                                            {'Test', 'jawoll'},
                                                             #check_value(self.form.application_areas.data),
                                                             {'Test', 'jawoll'},
                                                             check_value(self.form.image_url.data),
