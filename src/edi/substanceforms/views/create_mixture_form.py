e72715245b64fd44cdc996fa6e03405f3fe57126
Seppo Walther <seppo.walther@educorvi.de>
2021-10-07 10:51:59 +0200

    Fixing bug

--- src/edi/substanceforms/views/create_mixture_form.py
+++ src/edi/substanceforms/views/create_mixture_form.py
@@ -81,7 +81,7 @@ class CreateFormView(WTFormView):
                 cur = conn.cursor()
                 insert = """INSERT INTO substance_mixture VALUES (DEFAULT, '%s', '%s', %s, '%s', '%s', '%s', '%s', %s, %s, %s,
                          %s, %s, %s, %s, %s, '%s', %s, '%s',
-                          %s, '%s');""" % (self.form.title.data,
+                          %s, %s, %s, %s, '%s');""" % (self.form.title.data,
                                                             self.form.description.data,
                                                             check_value(self.form.branch.data),
                                                             self.context.aq_parent.get_webcode(),
@@ -99,9 +99,11 @@ class CreateFormView(WTFormView):
                                                             self.form.checked_emissions.data,
                                                             check_value(self.form.flashpoint.data),
                                                             self.form.values_range.data,
-                                                            #check_value(self.form.comments.data),
+                                                            check_value(self.form.comments.data),
                                                             #self.form.usecases.data,
-                                                            #self.form.application_areas.data,
+                                                            ['Test', 'jawoll'],
+                                                            #check_value(self.form.application_areas.data),
+                                                            ['Test', 'jawoll'],
                                                             check_value(self.form.image_url.data),
                                                             self.form.manufacturer_id.data)
                 cur.execute(insert)
