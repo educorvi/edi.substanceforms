fab8fefdb81fca59cbd3679197e9453b23374d31
Seppo Walther <seppo.walther@educorvi.de>
2021-10-13 09:37:14 +0200

    Working on create_substance_form.py

--- src/edi/substanceforms/views/create_substance_form.py
+++ src/edi/substanceforms/views/create_substance_form.py
@@ -4,6 +4,7 @@ from wtforms import validators
 from collective.wtforms.views import WTFormView
 from edi.substanceforms.helpers import check_value
 from edi.substanceforms.vocabularies import hskategorie, branchen
+from edi.substanceforms.views.create_mixture_form import MultiCheckboxField
 from plone import api as ploneapi
 import requests
 import psycopg2
@@ -35,31 +36,52 @@ class CreateFormView(WTFormView):
                     return result
         return self.index()
 
+    def create_image(self, image, title):
+        filedata = image.data.read()
+        filename = image.data.filename
+
+        blobimage = NamedBlobImage(data=filedata, filename=filename)
+        obj = ploneapi.content.create(type='Image', title=title, image=blobimage, container=self.context)
+
+        obj.indexObject()
+        transaction.commit()
+
+        return obj.UID()
+
     def submit(self, button):
+        image_url = ''
+        if self.form.image_url:
+            image_url = self.create_image(self.form.image_url, self.form.title.data)
+
         redirect_url = self.context.aq_parent.absolute_url()
         if button == 'Speichern': #and self.validate():
+            conn = psycopg2.connect(host=self.host, user=self.username, dbname=self.dbname, password=self.password)
+            cur = conn.cursor()
+            insert = """INSERT INTO substance VALUES (DEFAULT, '%s', '%s', '%s',
+                        %s, %s, %s, %s, %s);""" % (self.form.title.data,
+                                                       self.form.description.data,
+                                                       self.context.aq_parent.get_webcode(),
+                                                       check_value(self.form.casnr.data),
+                                                       self.form.concentration.data,
+                                                       check_value(self.form.skin_category.data),
+                                                       check_value(self.form.branch.data),
+                                                       check_value(image_url))
 
-            if True:
-                conn = psycopg2.connect(host=self.host, user=self.username, dbname=self.dbname, password=self.password)
-                cur = conn.cursor()
-                insert = """INSERT INTO substance VALUES (DEFAULT, '%s', '%s', '%s',
-                            %s, %s, %s, %s, %s);""" % (self.form.title.data,
-                                                           self.form.description.data,
-                                                           self.context.aq_parent.get_webcode(),
-                                                           check_value(self.form.casnr.data),
-                                                           self.form.concentration.data,
-                                                           check_value(self.form.skin_category.data),
-                                                           check_value(self.form.branch.data),
-                                                           check_value(self.form.image_id.data))
-
+            try:
                 cur.execute(insert)
                 conn.commit()
-                cur.close()
-                conn.close()
-                message=u'Der Gefahrstoff wurde erfolgreich gespeichert.'
-                ploneapi.portal.show_message(message=message, type='info', request=self.request)
-            #except:
-            #    print(u'Fehler beim Einfügen in die Datenbank')
+            except:
+                imageobj = ploneapi.content.get(UID=image_url)
+                ploneapi.content.delete(imageobj)
+
+                message = u'Fehler beim Hinzufügen des Gefahrstoffgemisches'
+                ploneapi.portal.show_message(message=message, type='error', request=self.request)
+
+            cur.close()
+            conn.close()
+            message=u'Der Gefahrstoff wurde erfolgreich gespeichert.'
+            ploneapi.portal.show_message(message=message, type='info', request=self.request)
+
             return self.request.response.redirect(redirect_url)
 
         elif button == 'Abbrechen':
