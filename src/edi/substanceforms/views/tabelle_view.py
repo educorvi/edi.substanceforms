23c6b6b398c1e90aee2866a7ee99c4b8df9a3b6e
Seppo Walther <seppo.walther@educorvi.de>
2021-10-14 13:55:07 +0200

    Working on tabelle_view.py

--- src/edi/substanceforms/views/tabelle_view.py
+++ src/edi/substanceforms/views/tabelle_view.py
@@ -39,7 +39,7 @@ class TabelleFormView(WTFormView):
 
         if view:
             url = "%s/@@%s" % (self.context.absolute_url(), viewname)
-            return self.request.response.redirect(url)
+            #return self.request.response.redirect(url)
 
         self.host = self.context.aq_parent.host
         self.dbname = self.context.aq_parent.database
