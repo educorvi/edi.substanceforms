f931ab47c1eaddf9838af49fb930e3cd5e8da160
Seppo Walther <seppo.walther@educorvi.de>
2021-10-14 14:50:24 +0200

    Working on selector_view.py

--- src/edi/substanceforms/views/selector_view.py
+++ src/edi/substanceforms/views/selector_view.py
@@ -5,7 +5,16 @@ from Products.Five.browser import BrowserView
 
 class SelectorView(BrowserView):
     def __call__(self):
-        template = '''<li class="heading" i18n:translate="">
-          Sample View
-        </li>'''
-        return template
+
+        viewname = "%s-form-view" % self.context.tablename
+        view = ploneapi.content.get_view(
+            name=viewname,
+            context=self.context,
+            request=self.request
+        )
+
+        if view:
+            url = "%s/@@%s" % (self.context.absolute_url(), viewname)
+            return self.request.response.redirect(url)
+
+        return self.request.response.redirect(self.context.absolute_url())
