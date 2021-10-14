af4fddd1ce7f2d73e4670763f56e50af968c797d
Seppo Walther <seppo.walther@educorvi.de>
2021-10-14 15:00:12 +0200

    Working on selector_view.py

--- src/edi/substanceforms/views/selector_view.py
+++ src/edi/substanceforms/views/selector_view.py
@@ -8,14 +8,15 @@ class SelectorView(BrowserView):
     def __call__(self):
 
         viewname = "%s-form-view" % self.context.tablename
-        view = ploneapi.content.get_view(
-            name=viewname,
-            context=self.context,
-            request=self.request
-        )
 
-        if view:
-            url = "%s/@@%s" % (self.context.absolute_url(), viewname)
+        try:
+            view = ploneapi.content.get_view(
+                name=viewname,
+                context=self.context,
+                request=self.request
+            )
+        except:
             return self.request.response.redirect(url)
 
+        url = "%s/@@%s" % (self.context.absolute_url(), viewname)
         return self.request.response.redirect(self.context.absolute_url())
