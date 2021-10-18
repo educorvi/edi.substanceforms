9603a8d8c0eb1ed3aaeb572f3e3799db965a1c46
Seppo Walther <seppo.walther@educorvi.de>
2021-10-18 23:31:54 +0200

    Trying to add create_ingredient_view

--- src/edi/substanceforms/views/create_ingredient_view.py
+++ src/edi/substanceforms/views/create_ingredient_view.py
@@ -57,6 +57,7 @@ class CreateIngredientForm(WTFormView):
         return self.formTemplate()
 
     def submit(self, button):
+        import pdb; pdb.set_trace()
         self.form.itemid.default = self.request.get('itemid')
         redirect_url = self.context.aq_parent.absolute_url()
         if button == 'Speichern': #and self.validate():
