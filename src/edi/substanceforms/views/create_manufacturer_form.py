eddaf587fdfd314e449a6a8a52573772d9082034
Seppo Walther <seppo.walther@educorvi.de>
2021-10-21 16:36:17 +0200

    Working on update-manufacturer

--- src/edi/substanceforms/views/create_manufacturer_form.py
+++ src/edi/substanceforms/views/create_manufacturer_form.py
@@ -10,9 +10,15 @@ import psycopg2
 
 class CreateForm(Form):
 
-    title = StringField("Titel", [validators.required()])
-    description = StringField("Beschreibung")
-    homepage = StringField("Homepage")
+    title = StringField("Titel", [validators.required()], render_kw={'class': 'form-control'})
+    description = StringField("Beschreibung", render_kw={'class': 'form-control'})
+    homepage = StringField("Homepage", render_kw={'class': 'form-control'})
+
+class UpdateForm(Form):
+
+    title = StringField("Titel", [validators.required()], render_kw={'class': 'form-control'})
+    description = StringField("Beschreibung", render_kw={'class': 'form-control'})
+    homepage = StringField("Homepage", render_kw={'class': 'form-control'})
 
 class CreateFormView(WTFormView):
     formClass = CreateForm
@@ -53,3 +59,50 @@ class CreateFormView(WTFormView):
         elif button == 'Abbrechen':
             return self.request.response.redirect(redirect_url)
 
+class UpdateFormView(CreateFormView):
+    formClass = UpdateForm
+
+    def __call__(self):
+        dbdata = self.context.aq_parent
+        self.db = DBConnect(host=dbdata.host, db=dbdata.database, user=dbdata.username, password=dbdata.password)
+        if self.submitted:
+            button = self.hasButtonSubmitted()
+            if button:
+                result = self.submit(button)
+                if result:
+                    return result
+        self.itemid = self.request.get('itemid')
+        getter = """SELECT title, description, homepage
+                    FROM %s WHERE %s_id = %s;""" % (self.context.tablename,
+                                                    self.context.tablename,
+                                                    self.itemid)
+        self.result = self.db.execute(getter)
+        self.db.close()
+        return self.index()
+
+    def renderForm(self):
+        self.form.title.default=self.result[0][0]
+        self.form.description.default=self.result[0][1]
+        self.form.homepage.default=self.result[0][2]
+        return self.formTemplate()
+
+    def submit(self, button):
+        """
+        """
+        redirect_url = self.context.aq_parent.absolute_url()
+        if button == 'Speichern': #and self.validate():
+            command = """UPDATE manufacturer SET title='%s', description='%s', homepage='%s'
+                         WHERE substance_id = %s;""" % (self.form.title.data,
+                                                        self.form.description.data,
+                                                        self.form.homepage.data)
+            self.db.execute(command)
+            message = u'Der Hersteller wurde erfolgreich aktualisiert.'
+            ploneapi.portal.show_message(message=message, type='info', request=self.request)
+            #message = u'Fehler beim Aktualisieren des Gefahrstoffgemisches'
+            #ploneapi.portal.show_message(message=message, type='error', request=self.request)
+
+            self.db.close()
+            return self.request.response.redirect(redirect_url)
+
+        elif button == 'Abbrechen':
+            return self.request.response.redirect(redirect_url)
