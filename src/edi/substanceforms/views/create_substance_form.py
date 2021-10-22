8286e1bba4ce4270d75cbd25d0a877580b0a11ec
Seppo Walther <seppo.walther@educorvi.de>
2021-10-22 10:16:24 +0200

    Working on delete-substance-button

--- src/edi/substanceforms/views/create_substance_form.py
+++ src/edi/substanceforms/views/create_substance_form.py
@@ -209,6 +209,7 @@ class DeleteFormView(CreateFormView):
         """
         """
         redirect_url = self.context.aq_parent.absolute_url()
+        import pdb; pdb.set_trace()
         if button == 'Speichern' and self.form.sure.data is True: #and self.validate():
             command = """DELETE FROM substance
                          WHERE substance_id = %s;""" % (self.form.item_id.data)
