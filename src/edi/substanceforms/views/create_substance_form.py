542dc31cf1b90fd743643db4ca9f7ad21a249d42
Seppo Walther <seppo.walther@educorvi.de>
2021-10-22 10:50:55 +0200

    Working on delete-substance-button

--- src/edi/substanceforms/views/create_substance_form.py
+++ src/edi/substanceforms/views/create_substance_form.py
@@ -201,7 +201,6 @@ class DeleteFormView(CreateFormView):
                     return result
         self.itemid = self.request.get('itemid')
         self.db.close()
-        """
         return self.index()
 
 
