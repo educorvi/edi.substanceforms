d2019689b2fdeca0abf12b7f97f829423e6076b0
Seppo Walther <seppo.walther@educorvi.de>
2021-12-02 13:38:27 +0100

    Re-adding of migration of substances without link

--- src/edi/substanceforms/views/migrationview.py
+++ src/edi/substanceforms/views/migrationview.py
@@ -256,6 +256,16 @@ class Migrationview(BrowserView):
                          reinstoff_systemisch, reinstoff_hinweise, reinstoff_link, reinstoff_published))
                 except:
                     pass
+            else:
+                cur = conn.cursor()
+                try:
+                    cur.execute(
+                        "INSERT INTO substance (title, webcode, casnr, egnr, skin_category, branch, dnel_lokal, dnel_systemisch, comments, link, published) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, NULL, %s);",
+                        (reinstoff_title, reinstoff_uid, reinstoff_casnr, reinstoff_egnr, reinstoff_skin,
+                         reinstoff_branche, reinstoff_lokal,
+                         reinstoff_systemisch, reinstoff_hinweise, reinstoff_published))
+                except:
+                    pass
             conn.commit()
             cur.close()
 
