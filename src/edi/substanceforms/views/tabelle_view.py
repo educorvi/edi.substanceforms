92a9e56e69b8c59b0d2ef726e034921f550a14bb
Seppo Walther <seppo.walther@educorvi.de>
2021-12-15 13:52:57 +0100

    Overwriting search field of base class for search field

--- src/edi/substanceforms/views/tabelle_view.py
+++ src/edi/substanceforms/views/tabelle_view.py
@@ -101,7 +101,7 @@ class HerstellerForm (BaseForm):
     manu = SelectField(u'Bitte wählen Sie einen Hersteller aus:', choices=[], render_kw={'class':'form-control'})
 
 class SubstanceForm (BaseForm):
-    search = StringField("Suchbegriff_lol", render_kw={'class':'form-control'})
+    search = StringField("Suchbegriff", render_kw={'class':'form-control'})
     casnr = IntegerField(u'Bitte geben Sie eine CAS-Nummer an:', render_kw={'class':'form-control'})
     concentration = IntegerField(u'Bitte geben Sie eine Konzentration in wässriger Lösung an:', render_kw={'class':'form-control'})
 
@@ -166,6 +166,12 @@ class SubstanceFormView(TabelleFormView):
     formClass = SubstanceForm
 
     def renderForm(self):
+        try:
+            insert = "SELECT substance_mixture_id, title FROM substance_mixture ORDER BY title;"
+            choices = self.db.execute(insert)
+        except:
+            choices = []
+        self.form.search.choices = choices
         self.form.process()
         return self.formTemplate()
 
