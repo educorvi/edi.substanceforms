1aa725867887b7c7121760f2e8a1d3687b5eb4ea
Seppo Walther <seppo.walther@educorvi.de>
2022-01-11 13:25:49 +0100

    Trying to fix bug in mixturemigration.py

--- src/edi/substanceforms/views/mixturemigration.py
+++ src/edi/substanceforms/views/mixturemigration.py
@@ -178,31 +178,52 @@ class Migrationview(BrowserView):
             etikett_usecases = i.get('verwendungszweck')
             try:
                 etikett_manufacturer_name = i.get('hersteller')['title']
+
+                etikett_review_state = i.get('review_state')
+
+                if etikett_review_state == 'published':
+                    etikett_published = 'published'
+                else:
+                    etikett_published = 'private'
+
+                cur = conn.cursor()
+                cur.execute(
+                    "SELECT manufacturer_id FROM manufacturer WHERE title = '{0}';".format(etikett_manufacturer_name))
+                etikett_manufacturer_id = cur.fetchall()
+                cur.close()
+
+                cur = conn.cursor()
+                # cur.execute("INSERT INTO manufacturer (title, description, webcode) VALUES (%s, %s, %s)") % (hersteller_title, hersteller_desc, hersteller_uid)
+                cur.execute(
+                    "INSERT INTO substance_mixture (title, description, webcode, branch, substance_type, image_url, skin_category, checked_emissions, flashpoint, values_range, usecases, manufacturer_id, status) VALUES (%s, %s, %s, 'druck_und_papier', 'label', NULL, %s, %s, %s, %s, %s, %s, %s);",
+                    (etikett_title, etikett_desc, etikett_uid, etikett_skin_category, etikett_checked_emissions,
+                     etikett_flashpoint, etikett_values_range, etikett_usecases, etikett_manufacturer_id[0], etikett_published))
+                conn.commit()
+                # print(etikett_title)  # correct
+                cur.close()
             except:
-                etikett_manufacturer_name = 'nichtvorhanden'
+                etikett_review_state = i.get('review_state')
 
-            etikett_review_state = i.get('review_state')
+                if etikett_review_state == 'published':
+                    etikett_published = 'published'
+                else:
+                    etikett_published = 'private'
 
-            if etikett_review_state == 'published':
-                etikett_published = 'published'
-            else:
-                etikett_published = 'private'
+                cur = conn.cursor()
+                cur.execute(
+                    "SELECT manufacturer_id FROM manufacturer WHERE title = '{0}';".format(etikett_manufacturer_name))
+                etikett_manufacturer_id = cur.fetchall()
+                cur.close()
 
-            cur = conn.cursor()
-            cur.execute(
-                "SELECT manufacturer_id FROM manufacturer WHERE title = '{0}';".format(etikett_manufacturer_name))
-            etikett_manufacturer_id = cur.fetchall()
-            cur.close()
-
-            cur = conn.cursor()
-            # cur.execute("INSERT INTO manufacturer (title, description, webcode) VALUES (%s, %s, %s)") % (hersteller_title, hersteller_desc, hersteller_uid)
-            cur.execute(
-                "INSERT INTO substance_mixture (title, description, webcode, branch, substance_type, image_url, skin_category, checked_emissions, flashpoint, values_range, usecases, manufacturer_id, status) VALUES (%s, %s, %s, 'druck_und_papier', 'label', NULL, %s, %s, %s, %s, %s, %s, %s);",
-                (etikett_title, etikett_desc, etikett_uid, etikett_skin_category, etikett_checked_emissions,
-                 etikett_flashpoint, etikett_values_range, etikett_usecases, etikett_manufacturer_id[0], etikett_published))
-            conn.commit()
-            # print(etikett_title)  # correct
-            cur.close()
+                cur = conn.cursor()
+                # cur.execute("INSERT INTO manufacturer (title, description, webcode) VALUES (%s, %s, %s)") % (hersteller_title, hersteller_desc, hersteller_uid)
+                cur.execute(
+                    "INSERT INTO substance_mixture (title, description, webcode, branch, substance_type, image_url, skin_category, checked_emissions, flashpoint, values_range, usecases, status) VALUES (%s, %s, %s, 'druck_und_papier', 'label', NULL, %s, %s, %s, %s, %s, %s);",
+                    (etikett_title, etikett_desc, etikett_uid, etikett_skin_category, etikett_checked_emissions,
+                     etikett_flashpoint, etikett_values_range, etikett_usecases, etikett_published))
+                conn.commit()
+                # print(etikett_title)  # correct
+                cur.close()
 
         print('Successfully migrated DETERGENT_LABELS')
 
