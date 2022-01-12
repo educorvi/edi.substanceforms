c4ff988284311992122d5513bb934bb67851936f
Seppo Walther <seppo.walther@educorvi.de>
2022-01-12 09:27:58 +0100

    Working on mixturemigration.py

--- src/edi/substanceforms/views/mixturemigration.py
+++ src/edi/substanceforms/views/mixturemigration.py
@@ -184,22 +184,25 @@ class Migrationview(BrowserView):
                 etikett_published = 'private'
 
             if i.get('hersteller'):
-                etikett_manufacturer_name = i.get('hersteller')['title']
-                cur = conn.cursor()
-                cur.execute(
-                    "SELECT manufacturer_id FROM manufacturer WHERE title = '{0}';".format(etikett_manufacturer_name))
-                etikett_manufacturer_id = cur.fetchall()
-                cur.close()
+                try:
+                    etikett_manufacturer_name = i.get('hersteller')['title']
+                    cur = conn.cursor()
+                    cur.execute(
+                        "SELECT manufacturer_id FROM manufacturer WHERE title = '{0}';".format(etikett_manufacturer_name))
+                    etikett_manufacturer_id = cur.fetchall()
+                    cur.close()
 
-                cur = conn.cursor()
-                # cur.execute("INSERT INTO manufacturer (title, description, webcode) VALUES (%s, %s, %s)") % (hersteller_title, hersteller_desc, hersteller_uid)
-                cur.execute(
-                    "INSERT INTO substance_mixture (title, description, webcode, branch, substance_type, image_url, skin_category, checked_emissions, flashpoint, values_range, usecases, manufacturer_id, status) VALUES (%s, %s, %s, 'druck_und_papier', 'label', NULL, %s, %s, %s, %s, %s, %s, %s);",
-                    (etikett_title, etikett_desc, etikett_uid, etikett_skin_category, etikett_checked_emissions,
-                     etikett_flashpoint, etikett_values_range, etikett_usecases, etikett_manufacturer_id[0], etikett_published))
-                conn.commit()
-                # print(etikett_title)  # correct
-                cur.close()
+                    cur = conn.cursor()
+                    # cur.execute("INSERT INTO manufacturer (title, description, webcode) VALUES (%s, %s, %s)") % (hersteller_title, hersteller_desc, hersteller_uid)
+                    cur.execute(
+                        "INSERT INTO substance_mixture (title, description, webcode, branch, substance_type, image_url, skin_category, checked_emissions, flashpoint, values_range, usecases, manufacturer_id, status) VALUES (%s, %s, %s, 'druck_und_papier', 'label', NULL, %s, %s, %s, %s, %s, %s, %s);",
+                        (etikett_title, etikett_desc, etikett_uid, etikett_skin_category, etikett_checked_emissions,
+                         etikett_flashpoint, etikett_values_range, etikett_usecases, etikett_manufacturer_id[0], etikett_published))
+                    conn.commit()
+                    # print(etikett_title)  # correct
+                    cur.close()
+                except:
+                    import pdb; pdb.set_trace()
             else:
                 cur = conn.cursor()
                 # cur.execute("INSERT INTO manufacturer (title, description, webcode) VALUES (%s, %s, %s)") % (hersteller_title, hersteller_desc, hersteller_uid)
