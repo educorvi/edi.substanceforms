b62c36469b744d586e687c2d8888cb736af30668
Seppo Walther <seppo.walther@educorvi.de>
2022-01-14 13:32:28 +0100

    Removing PDB

--- src/edi/substanceforms/views/mixturemigration.py
+++ src/edi/substanceforms/views/mixturemigration.py
@@ -366,10 +366,6 @@ class Migrationview(BrowserView):
             heatset_evap_170 = heatset_verdampfung[0].get('bahn_170')
             heatset_evap_180 = heatset_verdampfung[0].get('bahn_180')
 
-
-            import pdb; pdb.set_trace()
-
-
             if heatset_review_state == 'published':
                 heatset_published = 'published'
             else:
