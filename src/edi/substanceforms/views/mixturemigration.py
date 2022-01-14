ae7f88dfdc58d0dc91d9dc492d5db900c17676dc
Seppo Walther <seppo.walther@educorvi.de>
2022-01-14 14:12:14 +0100

    PDB

--- src/edi/substanceforms/views/mixturemigration.py
+++ src/edi/substanceforms/views/mixturemigration.py
@@ -191,6 +191,7 @@ class Migrationview(BrowserView):
                 hinweise_string = '@'.join(etikett_hinweise)
                 etikett_hinweise = hinweise_string
 
+            import pdb; pdb.set_trace()
 
             if etikett_review_state == 'published':
                 etikett_published = 'published'
