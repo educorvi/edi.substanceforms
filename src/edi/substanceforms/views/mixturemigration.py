1adab116aedbd49afe4f6442d74ce295c05a8f0d
Seppo Walther <seppo.walther@educorvi.de>
2022-01-20 10:57:22 +0100

    Editing string composition for application areas

--- src/edi/substanceforms/views/mixturemigration.py
+++ src/edi/substanceforms/views/mixturemigration.py
@@ -262,6 +262,10 @@ class Migrationview(BrowserView):
                 usecases_string = '@'.join(manuell_usecases)
                 manuell_usecases = usecases_string
 
+            if manuell_application_areas:
+                areas_string = '@'.join(manuell_application_areas)
+                manuell_application_areas = areas_string
+
             if manuell_review_state == 'published':
                 manuell_published = 'published'
             else:
@@ -287,7 +291,7 @@ class Migrationview(BrowserView):
                 cur = conn.cursor()
                 # cur.execute("INSERT INTO manufacturer (title, description, webcode) VALUES (%s, %s, %s)") % (hersteller_title, hersteller_desc, hersteller_uid)
                 cur.execute(
-                    "INSERT INTO substance_mixture (title, description, webcode, branch, substance_type, image_url, skin_category, checked_emissions, flashpoint, values_range, usecases, application_areas, manufacturer_id, status) VALUES (%s, %s, %s, 'druck_und_papier', 'offset', NULL, %s, %s, %s, %s, %s, %s, NULL, %s);",
+                    "INSERT INTO substance_mixture (title, description, webcode, branch, substance_type, image_url, skin_category, checked_emissions, flashpoint, values_range, usecases, application_areas, manufacturer_id, status) VALUES (%s, %s, %s, 'druck_und_papier', 'special', NULL, %s, %s, %s, %s, %s, %s, NULL, %s);",
                     (manuell_title, manuell_desc, manuell_uid, manuell_skin_category, manuell_checked_emissions,
                      manuell_flashpoint, manuell_values_range, manuell_usecases, manuell_application_areas,
                      manuell_published))
