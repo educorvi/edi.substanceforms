0fe82f927d1a7167867e638b79fdae68503f9c57
Seppo Walther <seppo.walther@educorvi.de>
2022-01-24 17:04:41 +0100

    Working on update-form (application_areas, usecases)

--- src/edi/substanceforms/views/create_mixture_form.py
+++ src/edi/substanceforms/views/create_mixture_form.py
@@ -282,8 +282,6 @@ class UpdateFormView(CreateFormView):
                     command = "SELECT application_area_name FROM application_areas WHERE application_area_id = %s" % v[1]
                     result = self.db.execute(command)
                     for m in neuevocab:
-                        import pdb;
-                        pdb.set_trace()
                         if m == result[0][0]:
                             neueliste.append(i)
                 #del newlist[::2]
