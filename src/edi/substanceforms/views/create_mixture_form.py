8cc6f9101110ef472899806d9b8602a2109a473d
Seppo Walther <seppo.walther@educorvi.de>
2022-01-24 17:08:38 +0100

    Working on update-form (application_areas, usecases)

--- src/edi/substanceforms/views/create_mixture_form.py
+++ src/edi/substanceforms/views/create_mixture_form.py
@@ -285,8 +285,8 @@ class UpdateFormView(CreateFormView):
                     if m == result[0][0]:
                         neueliste.append(i)
 
-                print(neueliste)
-                import pdb; pdb.set_trace()
+            print(neueliste)
+            import pdb; pdb.set_trace()
                 #del newlist[::2]
                 #import pdb; pdb.set_trace()
             message = u'Das Gefahrstoffgemisch wurde erfolgreich aktualisiert.'
