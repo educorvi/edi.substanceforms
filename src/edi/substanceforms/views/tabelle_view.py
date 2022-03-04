0db5b593edad4c8321ef22667308916554d9d71c
Seppo Walther <seppo.walther@educorvi.de>
2022-03-04 08:53:04 +0100

    Not displaying non published items

--- src/edi/substanceforms/views/tabelle_view.py
+++ src/edi/substanceforms/views/tabelle_view.py
@@ -60,7 +60,7 @@ class TabelleFormView(WTFormView):
         results = []
         searchkey = self.context.tablename + '_id'
         searchtable = self.context.tablename
-        select = "SELECT %s, title FROM %s ORDER BY title ASC;" % (searchkey, searchtable)
+        select = "SELECT %s, title FROM %s WHERE status = 'published' ORDER BY title ASC;" % (searchkey, searchtable)
         try:
             conn = psycopg2.connect(host=self.host, user=self.username, password=self.password, dbname=self.dbname)
             cur = conn.cursor()
