7f265478effaaaa58faef698f1b3daa3a34d9c99
Seppo Walther <seppo.walther@educorvi.de>
2022-03-24 09:55:27 +0100

    Trying to implement vocab translation

--- src/edi/substanceforms/views/tabelle_view.py
+++ src/edi/substanceforms/views/tabelle_view.py
@@ -119,6 +119,7 @@ class TabelleFormView(WTFormView):
         searchkey = self.context.tablename + '_id'
         searchtable = self.context.tablename
         resultcolumns = self.context.resultcolumns
+        import pdb; pdb.set_trace()
 
         if not resultcolumns:
             select = "SELECT %s, title FROM %s WHERE status = 'published' ORDER BY title ASC;" % (searchkey, searchtable)
