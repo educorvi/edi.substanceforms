78316590f2415b0bc5d66e382caf68ada3db4113
Seppo Walther <seppo.walther@educorvi.de>
2022-04-06 09:54:46 +0200

    Working on gefahrstoffjson view

--- src/edi/substanceforms/views/gefahrstoff.py
+++ src/edi/substanceforms/views/gefahrstoff.py
@@ -40,6 +40,8 @@ class Gefahrstoff(BrowserView):
             inhaltsstoff = {}
             select = "SELECT * FROM substance WHERE substance_id = %s" % inhalt[0][1]
             reinstoff = self.db.execute(select)
+            import pdb;
+            pdb.set_trace()
             inhaltsstoff['cas'] = reinstoff[0][4]
             inhaltsstoff['gefahrstoff'] = reinstoff[0][1]
             inhaltsstoff['anteil_min'] = inhalt[0][3]
