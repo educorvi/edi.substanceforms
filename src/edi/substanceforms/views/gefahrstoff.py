cdcddf01c2d710114dc6c848f34e5779fba37a2f
Seppo Walther <seppo.walther@educorvi.de>
2022-04-06 10:08:17 +0200

    Working on gefahrstoffjson view

--- src/edi/substanceforms/views/gefahrstoff.py
+++ src/edi/substanceforms/views/gefahrstoff.py
@@ -43,8 +43,7 @@ class Gefahrstoff(BrowserView):
             pdb.set_trace()
             inhaltsstoff['cas'] = reinstoff[0][4]
             inhaltsstoff['gefahrstoff'] = reinstoff[0][1]
-            inhaltsstoff['anteil_min'] = inhalt[3]
-            inhaltsstoff['anteil_max'] = inhalt[4]
+            inhaltsstoff['anteil'] = f">= {inhalt[3]} - <= {inhalt[4]}"
             inhaltsstoffe.append(inhaltsstoff)
 
         produktclass = "SELECT class_name FROM productclasses WHERE class_id = %s" % data1[0][27]
