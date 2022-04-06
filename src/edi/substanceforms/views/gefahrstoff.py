05a1557268ae663c67247be905e0cfdae4fbaf2f
Seppo Walther <seppo.walther@educorvi.de>
2022-04-06 14:06:01 +0200

    Working on gefahrstoffjson view

--- src/edi/substanceforms/views/gefahrstoff.py
+++ src/edi/substanceforms/views/gefahrstoff.py
@@ -9,7 +9,8 @@ class Gefahrstoff(BrowserView):
     def __call__(self):
         self.db = DBConnect(host=self.context.host, db=self.context.database, user=self.context.username, password=self.context.password)
 
-        gemischid = self.request.get('gemischid')
+        gemischid = self.request.get('gemischid')+
+        import pdb; pdb.set_trace()
         #gemischid = "https://emissionsarme-produkte.bgetem.de/datenbank-chemie-dp/wasch-und-reinigungsmittel-fuer-den-etikettendruck/biolon-xi-fluessig"
         if gemischid.startswith('https://'):
             select = "SELECT mixture_id FROM oldlinks WHERE link = '%s'" % gemischid
