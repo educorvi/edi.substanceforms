aa205f580c4e8e4f6ec8e628cc6a04ac5ba50fc6
Seppo Walther <seppo.walther@educorvi.de>
2022-04-06 09:10:37 +0200

    Working on gefahrstoffliste view

--- src/edi/substanceforms/views/gefahrstoff.py
+++ src/edi/substanceforms/views/gefahrstoff.py
@@ -1,11 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from Products.Five.browser import BrowserView
+import jsonlib
+from edi.substanceforms.lib import DBConnect
 
 
 class Gefahrstoff(BrowserView):
     def __call__(self):
-        template = '''<li class="heading" i18n:translate="">
-          Sample View
-        </li>'''
-        return template
+        self.db = DBConnect(host=self.context.host, db=self.context.database, user=self.context.username, password=self.context.password)
+
+        #gemischid = self.request.get('gefahrstoffid')
+        gemischid = input()
+        if gemischid.startswith('https://'):
+            select = "SELECT mixture_id FROM oldlinks WHERE link = %s" % gemischid
+            mixture_id = self.db.execute(select)
+        else:
+            mixture_id = gemischid.split('.')[-1]
+
+
+        import pdb; pdb.set_trace()
+
+        gefahrstoffdata = {}
+
+
+
+        return gefahrstoffdata
