52000873894b19ab36b3438d1d5c12cebf65dbad
Seppo Walther <seppo.walther@educorvi.de>
2022-07-19 13:58:07 +0200

    Working on ingredients

--- src/edi/substanceforms/views/create_ingredient_view.py
+++ src/edi/substanceforms/views/create_ingredient_view.py
@@ -49,7 +49,7 @@ class CreateIngredientForm(WTFormView):
 
     def alreadyselected(self):
         itemid = self.request.get('itemid')
-        select = "SELECT substance.title FROM substance, recipes, substance_mixture WHERE recipes.mixture_id = %s AND substance.substance_id = recipes.substance_id" % itemid
+        select = "SELECT DISTINCT substance.title FROM substance, recipes, substance_mixture WHERE recipes.mixture_id = %s AND substance.substance_id = recipes.substance_id" % itemid
         result = self.db.execute(select)
         if result:
             return result
