f7060e9b2e36c154f9c1997a52cb705642d544ba
Seppo Walther <seppo.walther@educorvi.de>
2022-07-20 13:40:03 +0200

    Working on deleting ingredients

--- src/edi/substanceforms/views/create_mixture_form.py
+++ src/edi/substanceforms/views/create_mixture_form.py
@@ -453,19 +453,20 @@ class DeleteIngredientsFormView(CreateFormView):
         """
         redirect_url = self.context.absolute_url() + '/single_view?item=' + self.form.item_id.data
         if button == 'Speichern': #and self.validate():
-            import pdb; pdb.set_trace()
-            for i in self.form.ingres.data:
-                command = "DELETE FROM recipes WHERE mixture_id = %s AND substance_id = %s" % (self.form.item_id.data, i)
-                self.db.execute(command)
-                message = u'Die Bestandteile wurden erfolgreich gelöscht'
-                ploneapi.portal.show_message(message=message, type='info', request=self.request)
+            if self.form.ingres.data:
+                import pdb; pdb.set_trace()
+                for i in self.form.ingres.data:
+                    command = "DELETE FROM recipes WHERE mixture_id = %s AND substance_id = %s" % (self.form.item_id.data, i)
+                    self.db.execute(command)
+                    message = u'Die Bestandteile wurden erfolgreich gelöscht'
+                    ploneapi.portal.show_message(message=message, type='info', request=self.request)
 
-                self.db.close()
-                return self.request.response.redirect(redirect_url)
+                    self.db.close()
+                    return self.request.response.redirect(redirect_url)
+            else:
+                message = u'Die Bestandteile wurden nicht gelöscht, da das Bestätigungsfeld nicht ausgewählt war.'
+                ploneapi.portal.show_message(message=message, type='error', request=self.request)
 
-        elif button == 'Speichern' and self.form.sure.data is False:
-            message = u'Die Bestandteile wurden nicht gelöscht, da das Bestätigungsfeld nicht ausgewählt war.'
-            ploneapi.portal.show_message(message=message, type='error', request=self.request)
 
         elif button == 'Abbrechen':
             return self.request.response.redirect(redirect_url)
