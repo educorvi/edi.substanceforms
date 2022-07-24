89a97f59265773c0fec7f5a236bb11b8123dbb94
Seppo Walther <seppo.walther@educorvi.de>
2022-07-24 17:35:13 +0200

    Working on csvexport

--- src/edi/substanceforms/views/csvexport.py
+++ src/edi/substanceforms/views/csvexport.py
@@ -17,40 +17,47 @@ class Csvexport(BrowserView):
         mixtureselect = "SELECT * FROM substance_mixture"
         mixtures = self.db.execute(mixtureselect)
 
-        for i in mixtures:
-            id = i[0]
-            title = i[1]
-            description = i[2]
-            webcode = i[3]
-            branch = i[4]
-            substance_type = i[5]
-            evap_150 = i[10]
-            evap_160 = i[11]
-            evap_170 = i[12]
-            evap_180 = i[13]
-            ueg = i[14]
-            response = i[15]
-            skin_category = i[16]
-            checked_emissions = i[17]
-            date_checked = i[18]
-            flashpoint = i[19]
-            values_range = i[20]
-            classifications = i[21]
-            indicators = i[22]
-            comments = i[23]
-            manufacturer_id = i[25]
-            status = i[26]
-            productclass = i[27]
+        with open('eggs.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(['ID', 'Titel', 'Beschreibung', 'Webcode', 'Branche', 'Typ des Gefahrstoffgemischs',
+                                 'Verdampfungsfaktor 150', 'Verdampfungsfaktor 160', 'Verdampfungsfaktor 170',
+                                 'Verdampfungsfaktor 180', 'UEG', 'Responsefaktor', 'Hautschutzmittelkategorie',
+                                 'Emissionsgeprüft', 'Prüfdatum', 'Flammpunkt', 'Wertebereich', 'Klassifikationen',
+                                 'Indikatoren', 'Kommentare', 'Hersteller', 'Status', 'Produktklasse'])
+            for i in mixtures:
+                id = i[0]
+                title = i[1]
+                description = i[2]
+                webcode = i[3]
+                branch = i[4]
+                substance_type = i[5]
+                evap_150 = i[10]
+                evap_160 = i[11]
+                evap_170 = i[12]
+                evap_180 = i[13]
+                ueg = i[14]
+                response = i[15]
+                skin_category = i[16]
+                checked_emissions = i[17]
+                date_checked = i[18]
+                flashpoint = i[19]
+                values_range = i[20]
+                classifications = i[21]
+                indicators = i[22]
+                comments = i[23]
+                manufacturer_id = i[25]
+                status = i[26]
+                productclass = i[27]
 
-            manufacturer = self.db.execute("SELECT title FROM manufacturer WHERE manufacturer_id = %s" % manufacturer_id)
-            newbranch = self.get_attr_translation('branch', branch)
-            newsubstancetype = self.get_attr_translation('substance_type', substance_type)
-            newskincategory = self.get_attr_translation('hskategorie', skin_category)
-            newchecked_emissions = self.get_attr_translation('boolvocab', checked_emissions)
-            newvalues_range = self.get_attr_translation('boolvocab', values_range)
+                manufacturer = (self.db.execute("SELECT title FROM manufacturer WHERE manufacturer_id = %s" % manufacturer_id))[0][0]
+                newbranch = self.get_attr_translation('branch', branch)
+                newsubstancetype = self.get_attr_translation('substance_type', substance_type)
+                newskincategory = self.get_attr_translation('hskategorie', skin_category)
+                newchecked_emissions = self.get_attr_translation('boolvocab', str(checked_emissions))
+                newvalues_range = self.get_attr_translation('boolvocab', str(values_range))
 
 
-            import pdb; pdb.set_trace()
+                import pdb; pdb.set_trace()
 
 
 
