fb6105b9668114447e17a14e3b2bd6c47294820a
Seppo Walther <seppo.walther@educorvi.de>
2022-08-16 14:49:26 +0200

    Working on csvexport

--- src/edi/substanceforms/views/csvexport.py
+++ src/edi/substanceforms/views/csvexport.py
@@ -317,15 +317,22 @@ class CsvmixtureNew(BrowserView):
                         singleentry.append(concentration_max)
                         entrylist.append(singleentry)
 
-                """
-                if len(entrylist) == 1:
-                    writer.writerow(
-                        [id, title, description, webcode, newbranch, newsubstancetype, newapplicationareas,
-                         newusecases, evap_150, evap_160, evap_170, evap_180,
-                         ueg, response, newskincategory, newchecked_emissions, date_checked, flashpoint,
-                         newvalues_range,
-                         newclassifications, newindicators, comments, manufacturer, status, newproductclass,
-                         entrylist[0][0][0][0], entrylist[0][1][0][0], entrylist[0][2], entrylist[0][3]])
+
+                if isinstance(entrylist, list) and len(entrylist) >= 1:
+                    for entry in entrylist:
+                        writer.writerow(
+                            [id, title, description, webcode, newbranch, newsubstancetype, newapplicationareas,
+                             newusecases, evap_150, evap_160, evap_170, evap_180,
+                             ueg, response, newskincategory, newchecked_emissions, date_checked, flashpoint,
+                             newvalues_range,
+                             newclassifications, newindicators, comments, manufacturer, status, newproductclass,
+                             entrylist[0][0][0], entrylist[1][0][0], entrylist[2], entrylist[3]])
+                        if len(entrylist) > 1:
+                            entrylist.pop(0)
+                            writer.writerow(
+                                [None, None, None, None, None, None, None, None, None, None, None, None, None,
+                                 None,None, None, None, None, None, None, None, None, None, None, None,
+                                 entrylist[0][0][0], entrylist[1][0][0], entrylist[2], entrylist[3]])
                 """
                 if isinstance(entrylist, list) and len(entrylist) >= 1:
                     for entry in entrylist:
@@ -336,7 +343,7 @@ class CsvmixtureNew(BrowserView):
                              newvalues_range,
                              newclassifications, newindicators, comments, manufacturer, status, newproductclass,
                              entry[0][0][0], entry[1][0][0], entry[2], entry[3]])
-
+                """
 
 
     def get_attr_translation(self, attribute, value):
