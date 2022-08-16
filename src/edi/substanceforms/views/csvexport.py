ae05b1884ca13ae1848f926ba5d57d97014f5d54
Seppo Walther <seppo.walther@educorvi.de>
2022-08-16 14:22:35 +0200

    Working on csvexport

--- src/edi/substanceforms/views/csvexport.py
+++ src/edi/substanceforms/views/csvexport.py
@@ -325,7 +325,7 @@ class CsvmixtureNew(BrowserView):
                          ueg, response, newskincategory, newchecked_emissions, date_checked, flashpoint,
                          newvalues_range,
                          newclassifications, newindicators, comments, manufacturer, status, newproductclass,
-                         singleentry[0][0], singleentry[0][1], singleentry[0][2], singleentry[0][3]])
+                         entrylist[0][0], entrylist[0][1], entrylist[0][2], entrylist[0][3]])
 
 
 
