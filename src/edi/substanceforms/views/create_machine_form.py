6196cfbc1c5efc260c33af79ba3dc7bc2a4e9c27
Lars Walther <larswalther@MacBook-Air-von-Lars.local>
2023-02-11 17:06:26 +0100

    create_ingredient, create_machine, create_machine, create_manufacturerer bereinigt

--- src/edi/substanceforms/views/create_machine_form.py
+++ src/edi/substanceforms/views/create_machine_form.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-from wtforms import Form, StringField, SelectField, IntegerField, FileField
+from wtforms import Form, StringField, SelectField, FileField
 from wtforms import validators
 from collective.wtforms.views import WTFormView
 from edi.substanceforms.helpers import check_value
 from plone import api as ploneapi
-import requests
-import psycopg2
+from edi.substanceforms.lib import DBConnect
 
 class CreateForm(Form):
 
@@ -19,10 +18,8 @@ class CreateFormView(WTFormView):
     buttons = ('Speichern', 'Abbrechen')
 
     def __call__(self):
-        self.host = self.context.aq_parent.host
-        self.dbname = self.context.aq_parent.database
-        self.username = self.context.aq_parent.username
-        self.password = self.context.aq_parent.password
+        dbdata = self.context.aq_parent
+        self.db = DBConnect(host=dbdata.host, db=dbdata.database, user=dbdata.username, password=dbdata.password)
         if self.submitted:
             button = self.hasButtonSubmitted()
             if button:
@@ -32,41 +29,28 @@ class CreateFormView(WTFormView):
         return self.index()
 
     def renderForm(self):
-        try:
-            conn = psycopg2.connect(host=self.host, user=self.username, dbname=self.dbname, password=self.password)
-            cur = conn.cursor()
-            cur.execute("SELECT manufacturer_id, title FROM manufacturer;")
-            manus = cur.fetchall()
-            cur.close
-            conn.close()
-        except:
-            manus = []
+        conn = self.db.connect()
+        select = "SELECT manufacturer_id, title FROM manufacturer;"
+        manus = conn.execute(select)
+        conn.close()
         self.form.manufacturer_id.choices = manus
         self.form.process()
         return self.formTemplate()
 
     def submit(self, button):
+        conn = self.db.connect()
         redirect_url = self.context.aq_parent.absolute_url()
-        if button == 'Speichern': #and self.validate():
-
-            if True:
-                conn = psycopg2.connect(host=self.host, user=self.username, dbname=self.dbname, password=self.password)
-                cur = conn.cursor()
-                insert = """INSERT INTO printing_machine VALUES (DEFAULT, '%s', '%s', '%s', 
-                            %s, %s);""" % (self.form.title.data, 
-                                           self.form.description.data,
-                                           self.context.aq_parent.get_webcode(),
-                                           check_value(self.form.image.data),
-                                           check_value(self.form.manufacturer_id.data))
-          
-                cur.execute(insert)
-                conn.commit()
-                cur.close()
-                conn.close()
-                message=u'Die Maschine wurde erfolgreich gespeichert.'
-                ploneapi.portal.show_message(message=message, type='info', request=self.request)
-            #except:
-            #    print(u'Fehler beim Einfügen in die Datenbank')
+        if button == 'Speichern':
+            insert = """INSERT INTO printing_machine VALUES (DEFAULT, '%s', '%s', '%s', 
+                        %s, %s);""" % (self.form.title.data, 
+                                       self.form.description.data,
+                                       self.context.aq_parent.get_webcode(),
+                                       check_value(self.form.image.data),
+                                       check_value(self.form.manufacturer_id.data))
+            conn.execute(insert)  
+            conn.close()
+            message=u'Die Maschine wurde erfolgreich gespeichert.'
+            ploneapi.portal.show_message(message=message, type='info', request=self.request)
             return self.request.response.redirect(redirect_url)
 
         elif button == 'Abbrechen':
--- src/edi/substanceforms/views/create_manufacturer_form.py
+++ src/edi/substanceforms/views/create_manufacturer_form.py
@@ -5,8 +5,6 @@ from collective.wtforms.views import WTFormView
 from edi.substanceforms.helpers import check_value
 from plone import api as ploneapi
 from edi.substanceforms.lib import DBConnect
-import requests
-import psycopg2
 
 class CreateForm(Form):
 
@@ -33,10 +31,6 @@ class CreateFormView(WTFormView):
     def __call__(self):
         dbdata = self.context.aq_parent
         self.db = DBConnect(host=dbdata.host, db=dbdata.database, user=dbdata.username, password=dbdata.password)
-        self.host = self.context.aq_parent.host
-        self.dbname = self.context.aq_parent.database
-        self.username = self.context.aq_parent.username
-        self.password = self.context.aq_parent.password
         if self.submitted:
             button = self.hasButtonSubmitted()
             if button:
@@ -46,25 +40,23 @@ class CreateFormView(WTFormView):
         return self.index()
 
     def submit(self, button):
+        conn = self.db.connect()
         redirect_url = self.context.aq_parent.absolute_url()
         if button == 'Speichern' and self.validate():
-
-            if True:
-                insert = """INSERT INTO manufacturer VALUES (DEFAULT, '%s', '%s', '%s', %s, %s);""" % (self.form.title.data,
-                                                            self.form.description.data,
-                                                            self.context.aq_parent.get_webcode(),
-                                                            check_value(self.form.homepage.data),
-                                                            check_value(self.form.status))
-                self.db.execute(insert)
-                self.db.close()
-                message=u'Der Hersteller wurde erfolgreich gespeichert.'
-                ploneapi.portal.show_message(message=message, type='info', request=self.request)
-            #except:
-            #    print(u'Fehler beim Einfügen in die Datenbank')
+            insert = """INSERT INTO manufacturer VALUES (DEFAULT, '%s', '%s', '%s', %s, %s);""" % (self.form.title.data,
+                                                                                                   self.form.description.data,
+                                                                                                   self.context.aq_parent.get_webcode(),
+                                                                                                   check_value(self.form.homepage.data),
+                                                                                                   check_value(self.form.status))
+            conn.execute(insert)
+            conn.close()
+            message=u'Der Hersteller wurde erfolgreich gespeichert.'
+            ploneapi.portal.show_message(message=message, type='info', request=self.request)
             return self.request.response.redirect(redirect_url)
 
         elif button == 'Abbrechen':
             return self.request.response.redirect(redirect_url)
+
 
 class UpdateFormView(CreateFormView):
     formClass = UpdateForm
@@ -83,8 +75,9 @@ class UpdateFormView(CreateFormView):
                     FROM %s WHERE %s_id = %s;""" % (self.context.tablename,
                                                     self.context.tablename,
                                                     self.itemid)
-        self.result = self.db.execute(getter)
-        self.db.close()
+        conn = self.db.connect()
+        self.result = conn.execute(getter)
+        conn.close()
         return self.index()
 
     def renderForm(self):
@@ -98,6 +91,7 @@ class UpdateFormView(CreateFormView):
     def submit(self, button):
         """
         """
+        conn = self.db.connect()
         redirect_url = self.context.absolute_url() + '/single_view?item=' + self.form.item_id.data
         if button == 'Speichern': #and self.validate():
             command = """UPDATE manufacturer SET title='%s', description='%s', homepage='%s'
@@ -105,17 +99,15 @@ class UpdateFormView(CreateFormView):
                                                         self.form.description.data,
                                                         self.form.homepage.data,
                                                         self.form.item_id.data)
-            self.db.execute(command)
+            conn.execute(command)
+            conn.close()
             message = u'Der Hersteller wurde erfolgreich aktualisiert.'
             ploneapi.portal.show_message(message=message, type='info', request=self.request)
-            #message = u'Fehler beim Aktualisieren des Gefahrstoffgemisches'
-            #ploneapi.portal.show_message(message=message, type='error', request=self.request)
-
-            self.db.close()
             return self.request.response.redirect(redirect_url)
 
         elif button == 'Abbrechen':
             return self.request.response.redirect(redirect_url)
+
 
 class DeleteFormView(CreateFormView):
     formClass = DeleteForm
@@ -130,9 +122,7 @@ class DeleteFormView(CreateFormView):
                 if result:
                     return result
         self.itemid = self.request.get('itemid')
-        self.db.close()
         return self.index()
-
 
     def renderForm(self):
         self.form.item_id.default=self.itemid
@@ -142,14 +132,14 @@ class DeleteFormView(CreateFormView):
     def submit(self, button):
         """
         """
+        conn = self.db.connect()
         redirect_url = self.context.aq_parent.absolute_url()
         if button == 'Speichern' and self.form.sure.data is True: #and self.validate():
             command = "DELETE FROM manufacturer WHERE manufacturer_id = %s" % (self.form.item_id.data)
-            self.db.execute(command)
+            conn.execute(command)
+            conn.close()
             message = u'Der Hersteller wurde erfolgreich gelöscht'
             ploneapi.portal.show_message(message=message, type='info', request=self.request)
-
-            self.db.close()
             return self.request.response.redirect(redirect_url)
 
         elif button == 'Speichern' and self.form.sure.data is False:
