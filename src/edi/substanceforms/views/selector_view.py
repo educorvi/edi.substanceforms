062728e9915e952bb4c09cfe1a71c877c351443e
Lars Walther <larswalther@MacBook-Air-von-Lars.local>
2023-02-11 16:25:21 +0100

    Update / Bereinigung selector_vew

--- src/edi/substanceforms/views/selector_view.py
+++ src/edi/substanceforms/views/selector_view.py
@@ -8,18 +8,10 @@ class SelectorView(BrowserView):
     def __call__(self):
 
         viewname = "%s-form-view" % self.context.tablename
-        #if self.context.tablename == 'substance_mixture':
-            #if self.context.nochzudefinieren == "Etiketten":
-                #viewname="etiketten-form-view"
-
-        try:
-            view = ploneapi.content.get_view(
-                name=viewname,
-                context=self.context,
-                request=self.request
-            )
-        except:
-            return self.request.response.redirect(self.context.absolute_url()+"/@@tabelle_view")
+        view = ploneapi.content.get_view(
+            name=viewname,
+            context=self.context,
+            request=self.request)
 
         url = "%s/@@%s" % (self.context.absolute_url(), viewname)
         return self.request.response.redirect(url)
