e6aba456796bd8dadb97ac334e2148101b2c2104
seppowalther <seppo.walther@educorvi.de>
2023-03-02 16:24:52 +0100

    Trying to fix bug with conn

--- src/edi/substanceforms/lib.py
+++ src/edi/substanceforms/lib.py
@@ -15,22 +15,18 @@ class DBConnect(object):
 
     def execute(self, command):
         results = False
-        conn = psycopg2.connect(host=self.host,
-                                dbname=self.db,
-                                user=self.user,
-                                password=self.password)
-        cur = conn.cursor()
+        connect()
+        cur = self.conn.cursor()
         cur.execute(command)
         method = command.split(" ")[0]
         if method == "SELECT":
             results = cur.fetchall()
         elif method in ["INSERT", "UPDATE"]:
-            results = conn.commit()
+            results = self.conn.commit()
         elif method == "DELETE":
-            results = conn.commit()
+            results = self.conn.commit()
         cur.close()
-        conn.close()
         return results
 
     def close(self):
-        conn.close()
+        self.conn.close()
