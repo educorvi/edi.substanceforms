bb0da3a5264d3f79ba621353a6f39a33ea0b4719
Seppo Walther <seppo.walther@educorvi.de>
2024-07-21 16:21:55 +0200

    Tabelle Viewlet in den Footer ausgelagert

--- src/edi/substanceforms/viewlets/substance_tabelle_viewlet.py
+++ src/edi/substanceforms/viewlets/substance_tabelle_viewlet.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
-
+from plone import api as ploneapi
 
 class SubstanceTabelleViewlet(ViewletBase):
 
     def update(self):
-        self.message = self.get_message()
+        pass
 
-    def get_message(self):
-        return u'My message'
+    def get_tablescript(self):
+        return ploneapi.portal.get().absolute_url() + '/++resource++edi.substanceforms/tabelle.js'
+
+    def get_searchscript(self):
+        return ploneapi.portal.get().absolute_url() + '/++resource++edi.substanceforms/search.js'
+
+    def get_hiddenscript(self):
+        return ploneapi.portal.get().absolute_url() + '/++resource++edi.substanceforms/hidden.js'
 
     def render(self):
         return super(SubstanceTabelleViewlet, self).render()
